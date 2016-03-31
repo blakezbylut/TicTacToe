f1e95ee59a9b0e368f369245634731d89c7f882c
Blake Zbylut <blakezbylut@gmail.com>
2016-03-30 20:46:52 -0400

    adds wireframe and readme files

---
+++ readme.md
@@ -0,0 +1,34 @@
+## Intro
+
+The game is Tic Tac Toe. The rules are obvious, but in case you are the outlier, you need three X's in a row to win as Player X, and three O's to win as Player O.
+
+The technologies used were jQuery, HTML, CSS and Javascript. The majority of the functions were run using jQuery. CSS allowed me to style the page, and I borrowed ideas (like the swing function on the buttons) from online posts. The HTML allowed me to put tags with id's and classes. I linked google fonts into the HTML file, along with the jQuery cdn library.
+
+## User Stories
+
+As a user:
+I would like to be able to click on the board.
+I would like to know whose turn it is.
+I would like when I click to see an X or O appear, based on whose turn it is.
+I would like to be alerted if either player wins.
+I would like to be alerted if it is a draw.
+I would like to be able to clear the board and reset the game to play again.
+I would like some kind of animation or aesthetic that engages me visually while I play.
+
+
+## Pseudocoding
+1. Player turns, in terms of Player X and Player O [✓]
+   -The first player that starts is Player X [✓]
+   -When a player clicks, it switches to the other players turn [✓]
+   -We want the page to display whose turn it is [✓]
+2. Click functions, if Player X clicks a box, the text that appears should be an X [✓]
+  -If Player O clicks a box, the text that appears should be an O [✓]
+  -If a player clicks the button “Clear the board”, the boxes should all reset to empty [✓]
+  -If a player clicks on a box that contains X or O, it should alert “unable to click here, click on another box.” [✓]
+3. The rules, to determine if the game is won, we must define certain conditions:
+If either of the three rows is all X, alert “the game is over - X is a winner”, and vice-versa for O [✓]
+If either of the three columns is all X or O, do the same [✓]
+If top-left to bottom-right diagonal is all X or O, repeat the alert [✓]
+If top-right to bottom-left diagonal is all X or O, repeat the alert [✓]
+If neither of the above conditions are met, and all the boxes are filled, alert “the game is a draw” [✓]
+4. Final decisions, once the game is won, the board should ignore inputs. A variable can track whether game is over or not, and execute code only if game is not yet over [✓]
--- scripts.js
+++ scripts.js
@@ -1,26 +1,26 @@
 console.log("Linked");
 
-var gameBoard = [
+var gameBoard = [//Here I create the main array that reflects the div boxes
 [null,null,null],
 [null,null,null],
 [null,null,null]
 ];
 
-var counter = 0;
-var nextTurn = "X";
-changeText();
+var counter = 0;//This is used to track whether it is a draw or not
+var nextTurn = "X";//This variable starts with a value of X, and changes depending on player turn
+changeText();//This runs text on the screen before the game(click) even begins
 
-var getRow = function(element) {
-    var row = element.attr('class').split(" ")[1];
+var getRow = function(element) {//Based on the id's given to the div boxes this serves to translate the div position to its equivalent in gameBoard array
+    var row = element.attr('class').split(" ")[1];//Not the most convenient way to grab a certain location...
     return parseInt(row.split("-")[1]);
 }
 
-var getCol = function(element) {
+var getCol = function(element) {//Repeat the process to grab column position
     var col = element.attr('class').split(" ")[2];
     return parseInt(col.split("-")[1]);
 }
 
-function changeTurn() {
+function changeTurn() {//The basic way to change between text on div and in array being X or O
     if(nextTurn == "X"){
       nextTurn = "O";
     } else {
@@ -28,22 +28,22 @@ function changeTurn() {
   }
 }
 
-function changeText() {
+function changeText() {//I was trying to setTimeout so that after 1 or 2 seconds the text would change, but I did not figure it out...
   $("#playerTurn").text("Player " + nextTurn + ": Please Click");
 }
 
-function winnerText(){
-  $("#winnerText").text("The winner is Player " + nextTurn + ". Congrats");
+function winnerText(){//Prints who the winner is
+  $("#winnerText").text("Player " + nextTurn + " wins. Congrats!");
 }
 
-$(".clearBoard").on("click", function(){//empty the array now
+$(".clearBoard").on("click", function(){//This resets the div contents, the array contents, turn the box-div off to reject input, and then calls click handler again to reset counter
   $(".box-div").text("");
   gameBoard = [
   [null,null,null],
   [null,null,null],
   [null,null,null]
   ];
-  // Sergey - WHY DOES THIS WORK, changeTurn and changeText need to be invoked twice?
+  // Sergey - WHY DOES THIS WORK, changeTurn and changeText need to be invoked twice? Here we are having issues with the nextTurn switching improperly when we clear the board
   nextTurn = "O";
   counter = 0;
   changeTurn();
@@ -54,7 +54,7 @@ $(".clearBoard").on("click", function(){//empty the array now
   $("#drawTime").text("");
 })
 
-function checkForWinner() {
+function checkForWinner() {//A series of conditionals to test for winner, based on array
   var isThereAWinner = false;
 
   if (gameBoard[0][0] !== null && (gameBoard[0][0] === gameBoard[0][1]) && (gameBoard[0][0] === gameBoard[0][2])){
@@ -77,40 +77,40 @@ function checkForWinner() {
   return isThereAWinner;
 }
 
-function setClickHandler() {
+function setClickHandler() {//The one, the only, click handler... We listen to the box-div class and we get the row/col based on target of the event
   $('.box-div').on("click", function(e) {
     var row = getRow($(e.target));
     var col = getCol($(e.target));
 
-    //checking to see if space is open
+    //Then, if space is open, we apply X or O to the text and also the corresponding array positon
     if(gameBoard[row - 1][col - 1] === null){
 
-        var move = nextTurn;//assigning move the X or O value
+        var move = nextTurn;//assigning move the X or O value, as a best practice method
 
-        if($(e.target).text("")){
-          $(e.target).text(nextTurn);
+        if($(e.target).text("")){//if the box-div is empty, fill it
+          $(e.target).text(move);
         }
-        gameBoard[row - 1][col - 1] = move;
-        counter++;
+        gameBoard[row - 1][col - 1] = move;//calculating array position
+        counter++;//we add to the counter so that if it hits 9 and no winner it is a draw
 
-        if(checkForWinner()){
+        if(checkForWinner()){//if winner function is true, turn off the box-divs and reset counter
           winnerText();
           $('.box-div').off();
           counter = 0;
-        } else if(counter == 9){
+        } else if(counter == 9){//tests for a draw
           $("#drawTime").text("We have a draw!");;
           counter = 0;
 
         }
-        changeTurn();
-        changeText();
+        changeTurn();//runs x and o
+        changeText();//changes text of whose turn it is
 
     } else{
-      alert("Please click somewhere else");
+      alert("Please click somewhere else");//if the posistion is not null run the alert
     }
   })
 }
 
-$(function() {
+$(function() {//the legendary onload.window function that runs only once the whole page is loaded
   setClickHandler();
 })
